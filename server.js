228e9229bdf92c35ab8f8a1ed00431272ef8e974
Charletontech <121673088+Charletontech@users.noreply.github.com>
2024-03-06 14:53:26 -0800

    Update server.js

--- server.js
+++ server.js
@@ -557,8 +557,18 @@ app.post('/customer-signup', (req, res) => {
 
   //LOGIC TO ADD USER TO DATABASE
   function addUserToDatabase(acctNoParam) {
-    var sql = `INSERT INTO customers (firstName, middleName, lastName, accountNumber, gender,  dob, email,  phoneNumber, password, phoneWorth, phoneModel, phoneBrand, phoneColor, address, plan, referrer) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
-    var values = [firstName, middleName, lastName, acctNoParam,  gender,  dob, email, phone, hashedPassword, worth, model,  brand, color, address,  plan,  referrer]
+    //logic to format time
+        let x = new Date().getHours()
+        let mins = new Date().getMinutes()
+        if ( x > 12) {
+          x -= 12
+          x += `:${mins}pm`
+        } else {
+          x += `:${mins}am`
+        }
+         //logic to format time ends
+    var sql = `INSERT INTO customers (time, firstName, middleName, lastName, accountNumber, gender,  dob, email,  phoneNumber, password, phoneWorth, phoneModel, phoneBrand, phoneColor, address, plan, referrer) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+    var values = [x, firstName, middleName, lastName, acctNoParam,  gender,  dob, email, phone, hashedPassword, worth, model,  brand, color, address,  plan,  referrer]
     connection.query(sql, values, (err, result1) => { 
       if (err) throw err
     })
